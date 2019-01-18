d13cb8be9bada52014cedb48ffa18024fdba43ad
Steven Than <steventhan11@gmail.com>
2019-01-17 22:58:50 -0500

    updated Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ class Home extends Component {
     render() {
         return (
             <div>
-                <h1>ASD 6150</h1>
+                <h1>ASD 6510</h1>
                 <h2>Hello world!</h2>
             </div>
         )
