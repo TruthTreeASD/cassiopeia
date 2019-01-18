8abb8f05644237864186963b9e79e5c8d05a0997
Steven Than <steventhan11@gmail.com>
2019-01-17 22:51:58 -0500

    first component

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>ASD 6150</h1>
+                <h2>Hello world!</h2>
+            </div>
+        )
+    }
+}
+
+export default Home;
