f2fd639b91dad1ef79fa3cbe6ec2159fd31f240b
Sheela Sathyanarayana <sheelasathyanarayana@gmail.com>
2019-01-20 18:53:18 -0500

    Header

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
+import Header from "./Header";
 
 class Home extends Component {
     render() {
         return (
-            <div>
-                <h1>ASD 6510</h1>
-                <h2>Hello world!</h2>
-            </div>
+       		<Header/>
+
         )
     }
 }
