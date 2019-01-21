f0e9ebd723775d9c4290275b9b5782cff2eb5051
yanevd <yanev.d@husky.neu.edu>
2019-01-20 21:53:54 -0800

    working on making a table
    table in progress

--- src/components/Red.js
+++ src/components/Red.js
@@ -5,15 +5,37 @@ import testTable from "../../src/testStuff/testTable.json";
 class Red extends Component {
 
     render() {
-        
-        var jjj = testTable[0]
+        var allColors = [];
+
+        for (var i in testTable) {
+            allColors.push(i, testTable[i]);
+            console.log(testTable[i]);
+
+        }
+
+        function get_table(data) {
+
+            let result = ['<table border=1>'];
+            
+                for (let cell of data) {
+                    result.push(`<td>${cell}</td>`);
+                }
+
+            result.push('</table>');
+            return result.join('\n');
+        }
 
         return (
             <div>
+                <p id="redTable"></p>
                 <label class="switch">
                     <input type="checkbox" />
                     <span class="slider round"></span>
                 </label>
+                <script type="text/javascript" language="JavaScript">
+                    get_table(allColors);
+                    alert("Hi there, and welcome.")
+                </script>
             </div>
         );
     }
