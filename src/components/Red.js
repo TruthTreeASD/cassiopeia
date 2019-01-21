3c63f2966c15e594f945a00e6e134d41804aa48e
yanevd <yanev.d@husky.neu.edu>
2019-01-20 19:33:17 -0800

    toggle switvh
    one switch

--- src/components/Red.js
+++ src/components/Red.js
@@ -1,38 +1,39 @@
 import React, { Component } from "react";
+import './Red.css';
 //import testTable from "../../public/testTable";
 
 class Red extends Component {
 
-    testTable = {
-    "colors": {
-        "red": {
-            "c1": 3,
-            "c2": 4,
-            "c3": 5
-        },
-        "blue": {
-            "c1": 30,
-            "c2": 40,
-            "c3": 50
-        },
-        "orange": {
-            "c1": -3,
-            "c2": -4,
-            "c3": -5
-        }
-    }
-};
-    output = document.getElementById('output');
-    //output.innerHTML = testTable.colors[0];
-
     render() {
-    return (
-        <div>
-            <tr>
-                <td></td>
-            </tr>
-        </div>
-    );
+        var testTable = {
+            "colors": {
+                "red": [3, 4, 5],
+                "blue": [30, 40, 50],
+                "orange": [-3, -4, -5]
+            }
+        };
+        var jjj = testTable[0]
+
+        function checkedBox() {
+            var checkBox = document.getElementById("myCheck");
+            var text = document.getElementById("text");
+            if (checkBox.checked == true) {
+                text.style.display = "block";
+            } else {
+                text.style.display = "none";
+            }
+        }
+
+
+
+        return (
+            <div>
+                <label class="switch">
+                    <input type="checkbox" />
+                    <span class="slider round"></span>
+                </label>
+            </div>
+        );
     }
 }
 
