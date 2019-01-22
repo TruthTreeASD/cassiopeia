4eb986580b1f881d47c9e8605d9fd5b51fab9d5f
Steven Than <steventhan11@gmail.com>
2019-01-22 01:08:13 -0500

    ran prettier on the entire project

--- src/components/FilterBy.js
+++ src/components/FilterBy.js
@@ -1,45 +1,48 @@
-import React, {Component} from 'react'
-import "../styles/FilterBy.css"
-import City from "../testStuff/cities.json"
-import Country from "../testStuff/counties.json"
+import React, { Component } from 'react';
+import '../styles/FilterBy.css';
+import City from '../testStuff/cities.json';
+import Country from '../testStuff/counties.json';
 
-
-
-class FilterBy extends Component{
-
-	 constructor(params) {
-     super(params) 
-     //initial dimension state set from props
-     this.state = {
-       dimension: this.props.dimension
-     }
-     this.setDimension = this.setDimension.bind(this)
+class FilterBy extends Component {
+  constructor(params) {
+    super(params);
+    //initial dimension state set from props
+    this.state = {
+      dimension: this.props.dimension
+    };
+    this.setDimension = this.setDimension.bind(this);
   }
-  
+
   setDimension(e) {
     this.setState({
       dimension: e.target.value
-    })
+    });
   }
- render() {
-    const {dimension} = this.state
-    return ( 
-
-    	<div className = "FilterBy">	
-        	Select the dimension to be filtered by:
-        	<div className = "RadioButtons">
-        	
-          	<input type="radio" checked={dimension == "City"} 
-				onClick={this.setDimension} value="City" /> City
-			
-			&nbsp;
-          	<input type="radio" checked={dimension == "County"} 
-				onClick={this.setDimension} value="County"  /> County
-        	</div>
-        { "Selected dimension: " } {dimension}
+  render() {
+    const { dimension } = this.state;
+    return (
+      <div className="FilterBy">
+        Select the dimension to be filtered by:
+        <div className="RadioButtons">
+          <input
+            type="radio"
+            checked={dimension == 'City'}
+            onClick={this.setDimension}
+            value="City"
+          />{' '}
+          City &nbsp;
+          <input
+            type="radio"
+            checked={dimension == 'County'}
+            onClick={this.setDimension}
+            value="County"
+          />{' '}
+          County
+        </div>
+        {'Selected dimension: '} {dimension}
       </div>
-      )
-	}
+    );
+  }
 }
 
 export default FilterBy;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,21 @@
-import React, { Component } from "react";
-import DisplayComponent from "./DisplayComponent";
-import Header from "./Header";
-import FilterBy from "./FilterBy";
-
+import React, { Component } from 'react';
+import DisplayComponent from './DisplayComponent';
+import Header from './Header';
+import FilterBy from './FilterBy';
 
 class Home extends Component {
-    render() {
-        return (
-            <div>
-              <Header/>
-                <h1>ASD 6510</h1>
-                <h2>Hello world!</h2>
-                <FilterBy/>
+  render() {
+    return (
+      <div>
+        <Header />
+        <h1>ASD 6510</h1>
+        <h2>Hello world!</h2>
+        <FilterBy />
 
-                <DisplayComponent></DisplayComponent>
-            </div>
-        )
-    }
+        <DisplayComponent />
+      </div>
+    );
+  }
 }
 
 export default Home;
