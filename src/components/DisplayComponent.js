af19b8821f524a15a8160fc309159b7ab0623c3b
Jhalaa <jhalaachinoy@gmail.com>
2019-01-26 00:17:39 -0800

    Adding rechats - first cut

--- src/components/DisplayComponent.js
+++ src/components/DisplayComponent.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend
+} from 'recharts';
 import '../styles/DisplayComponent.css';
 import Filters from './Filters';
+import testTable from '../../src/testStuff/cities.json';
 
 class DisplayComponent extends Component {
   constructor(props) {
@@ -30,7 +40,28 @@ class DisplayComponent extends Component {
           {' '}
           Chart{' '}
         </button>
-        <div className="DisplayArea">{this.state.displayComponent}</div>
+        <div className="DisplayArea">
+          <BarChart
+            width={600}
+            height={300}
+            data={[
+              { name: 'Page A', uv: 4000, pv: 2400, amt: 2400 },
+              { name: 'Page B', uv: 3000, pv: 1398, amt: 2210 },
+              { name: 'Page C', uv: 2000, pv: 9800, amt: 2290 },
+              { name: 'Page D', uv: 2780, pv: 3908, amt: 2000 },
+              { name: 'Page E', uv: 1890, pv: 4800, amt: 2181 },
+              { name: 'Page F', uv: 2390, pv: 3800, amt: 2500 },
+              { name: 'Page G', uv: 3490, pv: 4300, amt: 2100 }
+            ]}
+            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+          >
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="pv" fill="#8884d8" />
+          </BarChart>
+        </div>
         <div
           style={{
             display: 'flex',
--- src/components/LeftSideBar.js
+++ src/components/LeftSideBar.js
@@ -3,77 +3,71 @@ import '../styles/LeftSideBar.css';
 
 import testTable from '../../src/testStuff/cities.json';
 
-
 class LeftSideBar extends Component {
-    render() {
+  render() {
+    let allTableKeys = [];
 
-        let allTableKeys = [];
-        
-        for (let i in testTable) {
-            allTableKeys.push(i, testTable[i]);
-        }
-        for (let i = 0; i < allTableKeys.length; i++) {
-            if (i % 2 === 1) {
-                let elements = []
-                for (let j in allTableKeys[i]) {
-                    elements.push(j)//, testTable[i][j]
-                }
-                allTableKeys[i] = elements;
-            }
-        }
-        console.log(allTableKeys);
-
-        
-
-        let acc = document.getElementsByClassName("accordion");
-        let i;
-
-        for (i = 0; i < acc.length; i++) {
-            acc[i].addEventListener("click", function () {
-                console.log("Clicked!!")
-                this.classList.toggle("active");
-                let panel = this.nextElementSibling;
-                if (panel.style.display === "block") {
-                    panel.style.display = "none";
-                } else {
-                    panel.style.display = "block";
-                }
-            });
-        }
-
-        return (
-            <div>
-                
-                    {allTableKeys.map((answer, i) => {
-                        //console.log("Entered");
-                        // Return the element. Also pass key
-                        if (i % 2 == 0) {
-                            return (
-                                <div>
-                                    <button class="accordion">{answer}</button>
-                                    <div class="panel">
-                                        
-                                            {allTableKeys[i+1].map((elem, i) => {
-                                                return (<div>
-                                                    <p>{elem}
-                                                    <label className="switch">
-                                                        <input type="checkbox" />
-                                                        <span className="slider round" />
-                                                        </label></p>
-                                                </div>);
-                                            })}
-                                        
-                                    </div>
-                                </div>
-                            );
-                        }
-                    })}
-
-                
-
-            </div>
-        );
+    for (let i in testTable) {
+      allTableKeys.push(i, testTable[i]);
     }
+    for (let i = 0; i < allTableKeys.length; i++) {
+      if (i % 2 === 1) {
+        let elements = [];
+        for (let j in allTableKeys[i]) {
+          elements.push(j); //, testTable[i][j]
+        }
+        allTableKeys[i] = elements;
+      }
+    }
+    console.log(allTableKeys);
+
+    let acc = document.getElementsByClassName('accordion');
+    let i;
+
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener('click', function() {
+        console.log('Clicked!!');
+        this.classList.toggle('active');
+        let panel = this.nextElementSibling;
+        if (panel.style.display === 'block') {
+          panel.style.display = 'none';
+        } else {
+          panel.style.display = 'block';
+        }
+      });
+    }
+
+    return (
+      <div>
+        {allTableKeys.map((answer, i) => {
+          //console.log("Entered");
+          // Return the element. Also pass key
+          if (i % 2 == 0) {
+            return (
+              <div>
+                <button className="accordion">{answer}</button>
+                <div className="panel">
+                  {allTableKeys[i + 1].map((elem, i) => {
+                    return (
+                      <div>
+                        <p>
+                          {elem}
+                          <label className="switch">
+                            <input type="checkbox" />
+                            <span className="slider round" />
+                          </label>
+                        </p>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          }
+        })}
+      </div>
+    );
+  }
 }
 
 export default LeftSideBar;
