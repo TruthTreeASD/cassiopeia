6a6aab35b1ac2f0cbc0add34050dda46dc15f927
yanevd <yanev.d@husky.neu.edu>
2019-01-27 12:53:56 -0800

    added steven's code
    stevens code to left side bar works real well

--- src/components/LeftSideBar.js
+++ src/components/LeftSideBar.js
@@ -1,96 +1,54 @@
 import React, { Component } from 'react';
 import '../styles/LeftSideBar.css';
 
-import testTable from '../../src/testStuff/cities.json';
+import sidebarData from '../../src/testStuff/cities.json';
 
 
 class LeftSideBar extends Component {
     constructor(props) {
         super(props);
-        this.expandCollection = this.expandCollection.bind(this);
+        //   /api/collections?level=state
+        this.state = {};
+        // Set initial state of each collection to false
+        Object.keys(sidebarData).map(key => (this.state[key] = false));
     }
 
-    expandCollection(answer) {
-        console.log("Clicked!!", answer)
+    // Toggle state of each collection on click
+    handleClick = collection => {
+        console.log("Clicked!!", collection, this.state[collection]);
+        this.setState({ [collection]: !this.state[collection] });
+    };
 
-        /*this.classList.toggle("active");
-        let panel = answer.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
-       /* let acc = document.getElementsByClassName("accordion");
-        let i;
-
-        for (i = 0; i < acc.length; i++) {
-            
-                console.log("Clicked!!")
-                this.classList.toggle("active");
-                let panel = this.nextElementSibling;
-                if (panel.style.display === "block") {
-                    panel.style.display = "none";
-                } else {
-                    panel.style.display = "block";
-                }
- 
-        }*/
-    }
     render() {
-
-        let allTableKeys = [];
-        
-        for (let i in testTable) {
-            allTableKeys.push(i, testTable[i]);
-        }
-        for (let i = 0; i < allTableKeys.length; i++) {
-            if (i % 2 === 1) {
-                let elements = []
-                for (let j in allTableKeys[i]) {
-                    elements.push(j)//, testTable[i][j]
-                }
-                allTableKeys[i] = elements;
-            }
-        }
-        console.log(allTableKeys);
-
-        
-
-
 
         return (
             <div>
-                
-                    {allTableKeys.map((answer, i) => {
-                        //console.log("Entered");
-                        // Return the element. Also pass key
-                        if (i % 2 == 0) {
-                            return (
-                                <div>
-                                    <button class="accordionon" onClick={() => this.expandCollection(answer)}>{answer}</button>
-                                    <div class="panel">
-                                        
-                                            {allTableKeys[i+1].map((elem, i) => {
-                                                return (<div>
-                                                    <p>{elem}
-                                                    <label className="switch">
-                                                        <input type="checkbox" />
-                                                        <span className="slider round" />
-                                                        </label></p>
-                                                </div>);
-                                            })}
-                                        
-                                    </div>
-                                </div>
-                            );
-                        }
-                    })}
-
-                
-
+                {Object.keys(sidebarData).map((collection, i) => {
+                    return (
+                        <div>
+                            <div className="accordion"
+                                onClick={() => this.handleClick(collection)}>{collection}</div>
+                            <label style={{ display: this.state[collection] ? "block" : "none" }}>
+                                {Object.keys(sidebarData[collection]).map((attr, i) => {
+                                    return (
+                                        <div className="panel">
+                                            <p>{attr}
+                                                <label className="switch rightSide">
+                                                    <input type="checkbox" />
+                                                    <span className="slider round" />
+                                                </label>
+                                            </p>
+                                        </div>
+                                    );
+                                })}
+                            </label>
+                        </div>
+                    );
+                })}
             </div>
         );
     }
+    
 }
 
 export default LeftSideBar;
