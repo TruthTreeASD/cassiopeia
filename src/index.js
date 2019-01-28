92210b2ee34d38e5f1b10436be4cd1134614f78b
Steven Than <steventhan11@gmail.com>
2019-01-28 00:15:25 -0500

    added reactstrap for bootstrap 4 integration

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import configureStore from './store';
 
-import './index.css';
+import configureStore from './store';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
 ReactDOM.render(
   <Provider store={configureStore()}>
