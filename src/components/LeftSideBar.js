c9d7db27b00c15ecb3066821c70d16844169aa49
yanevd <yanev.d@husky.neu.edu>
2019-01-29 04:32:02 -0800

    fixed bug
    there was a click bug with the left side bar. fixed

--- src/components/LeftSideBar.js
+++ src/components/LeftSideBar.js
@@ -3,52 +3,56 @@ import '../styles/LeftSideBar.css';
 
 import sidebarData from '../../src/testStuff/cities.json';
 
-
 class LeftSideBar extends Component {
-    constructor(props) {
-        super(props);
-        //   /api/collections?level=state
-        this.state = {};
-        // Set initial state of each collection to false
-        Object.keys(sidebarData).map(key => (this.state[key] = false));
-    }
+  constructor(props) {
+    super(props);
+    //   /api/collections?level=state
+    this.state = {};
+    // Set initial state of each collection to false
+    Object.keys(sidebarData).map(key => (this.state[key] = false));
+  }
 
-    // Toggle state of each collection on click
-    handleClick = collection => {
-        console.log("Clicked!!", collection, this.state[collection]);
-        this.setState({ [collection]: !this.state[collection] });
-    };
+  // Toggle state of each collection on click
+  handleClick = collection => {
+    console.log('Clicked!!', collection, this.state[collection]);
+    this.setState({ [collection]: !this.state[collection] });
+  };
 
-    render() {
-
-        return (
+  render() {
+    return (
+      <div>
+        {Object.keys(sidebarData).map((collection, i) => {
+          return (
             <div>
-                {Object.keys(sidebarData).map((collection, i) => {
-                    return (
-                        <div>
-                            <div className="accordion"
-                                onClick={() => this.handleClick(collection)}>{collection}</div>
-                            <label style={{ display: this.state[collection] ? "block" : "none" }}>
-                                {Object.keys(sidebarData[collection]).map((attr, i) => {
-                                    return (
-                                        <div className="panel">
-                                            <p>{attr}
-                                                <label className="switch rightSide">
-                                                    <input type="checkbox" />
-                                                    <span className="slider round" />
-                                                </label>
-                                            </p>
-                                        </div>
-                                    );
-                                })}
-                            </label>
-                        </div>
-                    );
+              <div
+                className="accordion"
+                onClick={() => this.handleClick(collection)}
+              >
+                {collection}
+              </div>
+              <div
+                style={{ display: this.state[collection] ? 'block' : 'none' }}
+              >
+                {Object.keys(sidebarData[collection]).map((attr, i) => {
+                  return (
+                    <label className="panel">
+                      <p>
+                        {attr}
+                        <label className="switch rightSide">
+                          <input type="checkbox" />
+                          <span className="slider round" />
+                        </label>
+                      </p>
+                    </label>
+                  );
                 })}
+              </div>
             </div>
-        );
-    }
-    
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default LeftSideBar;
