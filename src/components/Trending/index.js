6e57b7c7d9380cde7f280c53e40d9ce0827a5644
Jhalaa <jhalaachinoy@gmail.com>
2019-02-10 15:01:51 -0800

    Adding links in trending page

--- src/components/Trending/index.js
+++ src/components/Trending/index.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import { Row } from 'reactstrap';
+import logo from '../../logo.png';
+import { Link } from 'react-router-dom';
 
 class Trending extends Component {
   render() {
     return (
       <Row className="flex-grow-1 flex-shrink-1">
-        <h2>Trending page</h2>
+        <img src={logo} size="100px" />
+        <Link to="/explore/">Explore</Link>
+        <div>Time Series</div>
       </Row>
     );
   }
