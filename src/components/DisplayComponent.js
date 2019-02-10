97c894dbf666f0f62cccfb8618b57be79aa80d1d
Jhalaa <jhalaachinoy@gmail.com>
2019-02-09 17:46:01 -0800

    Initial table commit with react virtualised

--- src/components/DisplayComponent.js
+++ src/components/DisplayComponent.js
@@ -1,60 +1,88 @@
 import React, { Component } from 'react';
 import '../styles/DisplayComponent.css';
 import classNames from 'classnames';
-import Filters from './AttributeRange';
-import CloroplethMap from './CloroplethMap';
-import CustomBarChart from './CustomBarChart';
+import 'react-virtualized/styles.css';
+import { defaultCellRangeRenderer, Grid } from 'react-virtualized';
 import { connect } from 'react-redux';
 
 class DisplayComponent extends Component {
   constructor(props) {
     super(props);
-    this.state = { displayComponent: 'Map' };
-    this.handleClick = this.handleClick.bind(this);
+    this.cellRenderer = this.cellRenderer.bind(this);
   }
 
-  handleClick(newState) {
-    this.setState({ displayComponent: newState });
+  // Grid data as an array of arrays
+
+  cellRenderer({ columnIndex, key, rowIndex, style }) {
+    let list = [
+      [
+        'Brian Vaughn',
+        'Software Engineer',
+        'San Jose',
+        'CA',
+        95125,
+        'Brian Vaughn',
+        'Software Engineer',
+        'San Jose'
+      ],
+      [
+        'Brian Vaughn',
+        'Software Engineer',
+        'San Jose',
+        'CA',
+        95125,
+        'Brian Vaughn',
+        'Software Engineer',
+        'San Jose'
+      ],
+      [
+        'Brian Vaughn',
+        'Software Engineer',
+        'San Jose',
+        'CA',
+        95125,
+        'Brian Vaughn',
+        'Software Engineer',
+        'San Jose'
+      ],
+      [
+        'Brian Vaughn',
+        'Software Engineer',
+        'San Jose',
+        'CA',
+        95125,
+        'Brian Vaughn',
+        'Software Engineer',
+        'San Jose'
+      ]
+      // And so on...
+    ];
+    return (
+      <div key={key} style={style}>
+        {list[rowIndex][columnIndex]}
+      </div>
+    );
+  }
+
+  customizedGrid(props) {
+    return (
+      <Grid
+        cellRenderer={this.cellRenderer}
+        columnCount={8}
+        columnWidth={150}
+        height={300}
+        rowCount={4}
+        rowHeight={30}
+        width={1100}
+        className="ReactVirtualized__Grid__innerScrollContainer"
+      />
+    );
   }
 
   render() {
-    var display;
-    var mapBtn = classNames('displayButtons', {
-      'btn-primary': this.state.displayComponent === 'Map'
-    });
-    var ChartBtn = classNames('displayButtons', {
-      'btn-primary': this.state.displayComponent === 'Chart'
-    });
-    if (this.state.displayComponent === 'Chart') {
-      display = <CustomBarChart />;
-    } else {
-      display = <CloroplethMap data={[['CA', 70]]} />;
-    }
     return (
       <div className="col-12 col-md-10 align-items-center">
-        <div class="display">
-          <button className={mapBtn} onClick={() => this.handleClick('Map')}>
-            {' '}
-            Map{' '}
-          </button>
-          <button
-            className={ChartBtn}
-            onClick={() => this.handleClick('Chart')}
-          >
-            {' '}
-            Chart{' '}
-          </button>
-          <div className="displayArea">{display}</div>
-          {/* <div
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center'
-          }}
-        >
-          
-        </div>*/}
-        </div>
+        {this.customizedGrid([1, 2, 3])}
       </div>
     );
   }
