04ae6606dbc7af55ea704e028338e8f771de7239
Jhalaa <jhalaachinoy@gmail.com>
2019-02-11 09:11:04 -0800

    Fixing the table component and moving it to display

--- src/components/DisplayComponent.js
+++ src/components/DisplayComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import classNames from 'classnames';
-import { defaultCellRangeRenderer, MultiGrid } from 'react-virtualized';
+import { Grid } from 'react-virtualized';
 import { connect } from 'react-redux';
 import axios from 'axios/index';
 import _ from 'lodash';
@@ -11,32 +11,75 @@ import '../styles/DisplayComponent.css';
 class DisplayComponent extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      currentLevel: null,
+      data: [['Name', 'Population']]
+    };
     this.cellRenderer = this.cellRenderer.bind(this);
   }
 
   cellRenderer({ columnIndex, key, rowIndex, style }) {
     return (
       <div key={key} style={style}>
-        {this.props.data[rowIndex][columnIndex]}
+        {this.state.data[rowIndex][columnIndex]}
       </div>
     );
   }
 
+  componentDidMount() {
+    let minPopulation = 0;
+    let maxPopulation = 0;
+    let data = [['Name', 'Population']];
+
+    // Calculate min and max population
+    axios
+      .get('/api/' + this.props.level + '/' + this.props.id)
+      .then(response => {
+        let population = parseFloat(response.data.population.replace(/,/g, ''));
+        maxPopulation = Math.floor(population * 1.5);
+        minPopulation = Math.floor(population * 0.5);
+        return axios
+          .get(
+            '/api/states?populationRange=' + minPopulation + ',' + maxPopulation
+          )
+          .then(response => {
+            _.map(response.data, obj => {
+              data.push([obj.name, obj.population]);
+            });
+            this.setState({ data: data });
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    // axios
+    //     .get('/api/attributes?value='+allstatesids+'&attributes='+selectedAttributesString+'&yearList='+selectedYearString)
+    //     .then(response => {
+    //         //data contains the variables
+    //         console.log(response.data);
+    //         this.setState({
+    //             sidebarData: response.data,
+    //             isLoaded: true
+    //         });
+    //     })
+    //     .catch(error => {
+    //         console.log(error);
+    //     });
+  }
+
   render() {
-    console.log('+++++++');
-    console.log(this.props);
-    console.log('+++++++');
     return (
-      <div id="mainDisplay" className="col-12 col-md-10 align-items-center">
-        <MultiGrid
+      <div id="mainDisplay">
+        <Grid
           cellRenderer={this.cellRenderer}
-          columnCount={this.props.data[0].length}
+          columnCount={this.state.data[0].length}
           columnWidth={150}
-          fixedColumnCount={2}
-          fixedRowCount={1}
           height={500}
-          rowCount={this.props.data.length}
-          rowHeight={30}
+          rowCount={this.state.data.length}
+          rowHeight={35}
           width={1100}
         />
       </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,67 +2,15 @@ import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import { connect } from 'react-redux';
 import '../styles/Home.css';
-import _ from 'lodash';
 
 import DisplayComponent from './DisplayComponent';
 import LeftSideBar from './LeftSideBar';
 import YearSelector from './YearSelector';
 import Filters from './AttributeRange';
-import axios from 'axios/index';
 
 class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      currentLevel: null,
-      data: [['Name', 'Population']]
-    };
-  }
-
-  componentDidMount() {
-    let currentLevel = this.props.match.params.level;
-    let id = this.props.match.params.id;
-    let minPopulation = 0;
-    let maxPopulation = 0;
-    let data = this.state.data;
-
-    // Calculate min and max population
-    axios
-      .get('/api/' + currentLevel + '/' + id)
-      .then(response => {
-        let population = parseFloat(response.data.population.replace(/,/g, ''));
-        maxPopulation = Math.floor(population * 1.5);
-        minPopulation = Math.floor(population * 0.5);
-        return axios
-          .get(
-            '/api/states?populationRange=' + minPopulation + ',' + maxPopulation
-          )
-          .then(response => {
-            _.map(response.data, obj => {
-              data.push([obj.name, obj.population]);
-            });
-            this.setState({ data: data });
-          })
-          .catch(error => {
-            console.log(error);
-          });
-      })
-      .catch(error => {
-        console.log(error);
-      });
-    // axios
-    //     .get('/api/attributes?value='+allstatesids+'&attributes='+selectedAttributesString+'&yearList='+selectedYearString)
-    //     .then(response => {
-    //         //data contains the variables
-    //         console.log(response.data);
-    //         this.setState({
-    //             sidebarData: response.data,
-    //             isLoaded: true
-    //         });
-    //     })
-    //     .catch(error => {
-    //         console.log(error);
-    //     });
   }
 
   render() {
@@ -70,7 +18,10 @@ class Home extends Component {
       <Row className="flex-grow-1 flex-shrink-1">
         <LeftSideBar />
         <div className="col-12 col-md-10 align-items-center">
-          <DisplayComponent data={this.state.data} />
+          <DisplayComponent
+            level={this.props.match.params.level}
+            id={this.props.match.params.id}
+          />
           <Row className="align-items-center">
             <Col sm={{ size: 6, order: 1, offset: 1 }}>
               <Filters />
