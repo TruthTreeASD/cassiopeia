9c1d0dd5c2e0aa80c9237ffddf3f31de457a203c
Jhalaa <jhalaachinoy@gmail.com>
2019-02-11 07:21:01 -0800

    Add calls to get data

--- src/components/DisplayComponent.js
+++ src/components/DisplayComponent.js
@@ -5,7 +5,7 @@ import { connect } from 'react-redux';
 import axios from 'axios/index';
 import _ from 'lodash';
 
-import 'react-virtualized/styles.css';
+import styles from 'react-virtualized/styles.css';
 import '../styles/DisplayComponent.css';
 
 class DisplayComponent extends Component {
@@ -14,87 +14,31 @@ class DisplayComponent extends Component {
     this.cellRenderer = this.cellRenderer.bind(this);
   }
 
-  componentWillReceiveProps() {
-    // Get the current level by parsing the url
-    let currentLevel = window.location.pathname.split('/')[2];
-    let id = window.location.pathname.split('/')[4];
-    let minPopulation = 0;
-    let maxPopulation = 0;
-    let data = [['Name', 'Population']];
-
-    // Calculate min and max population
-    axios
-      .get('/api/' + currentLevel + '/' + id)
-      .then(response => {
-        let population = parseFloat(response.data.population.replace(/,/g, ''));
-        maxPopulation = Math.floor(population * 1.5);
-        minPopulation = Math.floor(population * 0.5);
-        return axios
-          .get(
-            '/api/states?populationRange=' + minPopulation + ',' + maxPopulation
-          )
-          .then(response => {
-            _.map(response.data, obj => {
-              data.push([obj.name, obj.population]);
-            });
-            this.setState({ data: data });
-          })
-          .catch(error => {
-            console.log(error);
-          });
-      })
-      .catch(error => {
-        console.log(error);
-      });
-
-    this.setState({ currentLevel: currentLevel });
-    // axios
-    //     .get('/api/attributes?value='+allstatesids+'&attributes='+selectedAttributesString+'&yearList='+selectedYearString)
-    //     .then(response => {
-    //         //data contains the variables
-    //         console.log(response.data);
-    //         this.setState({
-    //             sidebarData: response.data,
-    //             isLoaded: true
-    //         });
-    //     })
-    //     .catch(error => {
-    //         console.log(error);
-    //     });
-  }
-
-  // Grid data as an array of arrays
-
   cellRenderer({ columnIndex, key, rowIndex, style }) {
-    console.log(this);
-    // let list = this.state.data;
-    // return (
-    //   <div key={key} style={style}>
-    //     {list[rowIndex][columnIndex]}
-    //   </div>
-    // );
-  }
-
-  customizedGrid(props) {
     return (
-      <MultiGrid
-        cellRenderer={this.cellRenderer}
-        columnCount={8}
-        columnWidth={150}
-        fixedColumnCount={2}
-        fixedRowCount={1}
-        height={500}
-        rowCount={4}
-        rowHeight={30}
-        width={1100}
-      />
+      <div key={key} style={style}>
+        {this.props.data[rowIndex][columnIndex]}
+      </div>
     );
   }
 
   render() {
+    console.log('+++++++');
+    console.log(this.props);
+    console.log('+++++++');
     return (
-      <div className="col-12 col-md-10 align-items-center">
-        {this.customizedGrid([1, 2, 3])}
+      <div id="mainDisplay" className="col-12 col-md-10 align-items-center">
+        <MultiGrid
+          cellRenderer={this.cellRenderer}
+          columnCount={this.props.data[0].length}
+          columnWidth={150}
+          fixedColumnCount={2}
+          fixedRowCount={1}
+          height={500}
+          rowCount={this.props.data.length}
+          rowHeight={30}
+          width={1100}
+        />
       </div>
     );
   }
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,19 +2,77 @@ import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import { connect } from 'react-redux';
 import '../styles/Home.css';
+import _ from 'lodash';
 
 import DisplayComponent from './DisplayComponent';
 import LeftSideBar from './LeftSideBar';
 import YearSelector from './YearSelector';
 import Filters from './AttributeRange';
+import axios from 'axios/index';
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentLevel: null,
+      data: [['Name', 'Population']]
+    };
+  }
+
+  componentWillMount() {
+    // Get the current level by parsing the url
+    let currentLevel = window.location.pathname.split('/')[2];
+    let id = window.location.pathname.split('/')[4];
+    let minPopulation = 0;
+    let maxPopulation = 0;
+    let data = this.state.data;
+
+    // Calculate min and max population
+    axios
+      .get('/api/' + currentLevel + '/' + id)
+      .then(response => {
+        let population = parseFloat(response.data.population.replace(/,/g, ''));
+        maxPopulation = Math.floor(population * 1.5);
+        minPopulation = Math.floor(population * 0.5);
+        return axios
+          .get(
+            '/api/states?populationRange=' + minPopulation + ',' + maxPopulation
+          )
+          .then(response => {
+            _.map(response.data, obj => {
+              data.push([obj.name, obj.population]);
+            });
+            this.setState({ data: data });
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    this.setState({ currentLevel: currentLevel });
+    // axios
+    //     .get('/api/attributes?value='+allstatesids+'&attributes='+selectedAttributesString+'&yearList='+selectedYearString)
+    //     .then(response => {
+    //         //data contains the variables
+    //         console.log(response.data);
+    //         this.setState({
+    //             sidebarData: response.data,
+    //             isLoaded: true
+    //         });
+    //     })
+    //     .catch(error => {
+    //         console.log(error);
+    //     });
+  }
+
   render() {
     return (
       <Row className="flex-grow-1 flex-shrink-1">
         <LeftSideBar />
         <div className="col-12 col-md-10 align-items-center">
-          <DisplayComponent />
+          <DisplayComponent data={this.state.data} />
           <Row className="align-items-center">
             <Col sm={{ size: 6, order: 1, offset: 1 }}>
               <Filters />
