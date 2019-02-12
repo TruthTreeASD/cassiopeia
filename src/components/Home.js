6509bdfc62e0c05c6de4b0037cbc9730e5caa092
sthan <sthan@tripadvisor.com>
2019-02-11 22:59:52 -0500

    made Home and Trending components responsive

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,33 +9,31 @@ import YearSelector from './YearSelector';
 import Filters from './AttributeRange';
 
 class Home extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
-      <Row className="flex-grow-1 flex-shrink-1">
-        <LeftSideBar />
-        <div className="col-12 col-md-10 align-items-center">
-          <DisplayComponent
-            level={this.props.match.params.level}
-            id={this.props.match.params.id}
-          />
-          <Row className="align-items-center">
-            <Col sm={{ size: 6, order: 1, offset: 1 }}>
-              <Filters
-                level={this.props.match.params.level}
-                location={this.props.match.params.name}
-                locationId={this.props.match.params.id}
-              />
-            </Col>
-            <Col sm={{ size: 4, order: 2, offset: 0 }}>
-              <YearSelector />
-            </Col>
-          </Row>
-        </div>
-      </Row>
+      <Container fluid>
+        <Row>
+          <LeftSideBar />
+          <div className="col-12 col-md-10 align-items-center">
+            <DisplayComponent
+              level={this.props.match.params.level}
+              id={this.props.match.params.id}
+            />
+            <Row className="align-items-center">
+              <Col sm={{ size: 6, order: 1, offset: 1 }}>
+                <Filters
+                  level={this.props.match.params.level}
+                  location={this.props.match.params.name}
+                  locationId={this.props.match.params.id}
+                />
+              </Col>
+              <Col sm={{ size: 4, order: 2, offset: 0 }}>
+                <YearSelector />
+              </Col>
+            </Row>
+          </div>
+        </Row>
+      </Container>
     );
   }
 }
