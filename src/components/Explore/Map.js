348525a1db651a410896fbbac4288b7d1bbf6611
sthan <sthan@tripadvisor.com>
2019-02-13 09:27:40 -0500

    Map component is ready for markers integration, just waiting on lat, long data from api

--- src/components/Explore/Map.js
+++ src/components/Explore/Map.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
+import { connect } from 'react-redux';
 
 import mapStyles from './mapStyles.json';
 
 class Map extends Component {
   state = {
+    markers: [],
     center: {
       lat: 37.09024,
       lng: -95.712891
@@ -13,13 +15,34 @@ class Map extends Component {
   };
 
   handleApiLoaded = (map, maps) => {
-    console.log(map, maps);
+    // console.log(map, maps);
     map.setOptions({
       styles: mapStyles
     });
+    this.map = map;
+    this.maps = maps;
   };
 
+  componentWillReceiveProps(nextProps) {
+    const geocoder = new this.maps.Geocoder();
+    const markersPromises = nextProps.suggestions.map(currentSuggestion => {
+      return new Promise((resolve, reject) => {
+        geocoder.geocode({ address: currentSuggestion }, (results, status) => {
+          if (status === 'OK') {
+            console.log(results);
+            resolve(results[0].geometry.location.lat());
+          } else {
+            reject(results);
+          }
+        });
+      });
+    });
+    Promise.all(markersPromises).then(markers => console.log(markers));
+    // this.setState({ markers });
+  }
+
   render() {
+    console.log(this.state);
     return (
       <GoogleMapReact
         bootstrapURLKeys={{ key: 'AIzaSyD6S0fXIE22l64bV3nhv-EB-7K1gh39nqs' }}
@@ -31,4 +54,8 @@ class Map extends Component {
     );
   }
 }
-export default Map;
+
+const mapStateToProps = store => ({
+  suggestions: store.LocationSearchBoxReducer
+});
+export default connect(mapStateToProps)(Map);
