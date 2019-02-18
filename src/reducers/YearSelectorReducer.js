976a2d1fd12f39a2f80042fa4897ab514cd7ae40
Sheela Sathyanarayana <sheelasathyanarayana@gmail.com>
2019-02-18 16:32:32 -0500

    Changed default year to 2016 and dropdown to end at 2016

--- src/reducers/YearSelectorReducer.js
+++ src/reducers/YearSelectorReducer.js
@@ -1,4 +1,4 @@
-export default (state = { yearSelected: 2019 }, action) => {
+export default (state = { yearSelected: 2016 }, action) => {
   switch (action.type) {
     case 'CHANGE_YEAR':
       //console.log(action);
