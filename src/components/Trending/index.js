5bba2d55802017871c7a5ec84bb4b9db751c3576
Sheela Sathyanarayana <sheelasathyanarayana@gmail.com>
2019-02-20 17:47:25 -0500

    test removed

--- src/components/Trending/index.js
+++ src/components/Trending/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
-import TimeSeries from '../Explore/TimeSeries';
 
 class Trending extends Component {
   render() {
