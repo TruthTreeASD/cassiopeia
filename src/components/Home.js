c17c8b6f9347495e035c84da3bdef81cb56f3f01
Sheela Sathyanarayana <sheelasathyanarayana@gmail.com>
2019-02-20 16:44:18 -0500

    Toggling views using tabs

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,7 @@ import DisplayComponent from './DisplayComponent';
 import LeftSideBar from './LeftSideBar';
 import YearSelector from './YearSelector';
 import Filters from './AttributeRange';
+import Tabs from './Explore/Tabs';
 
 class Home extends Component {
   render() {
@@ -17,10 +18,18 @@ class Home extends Component {
 
           <div className="col-12 col-md-10 align-items-center">
             <Card>
-              <DisplayComponent
-                level={this.props.match.params.level}
-                id={this.props.match.params.id}
-              />
+              <Tabs>
+                <div label="Table View">
+                  <DisplayComponent
+                    level={this.props.match.params.level}
+                    id={this.props.match.params.id}
+                  />
+                </div>
+
+                <div label="Time Series View">Coming soon :)</div>
+
+                <div label="Map View">Coming soon :)</div>
+              </Tabs>
             </Card>
 
             <Card>
