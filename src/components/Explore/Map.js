1d3ea38ebb2daefd0799e3a21273fc4c6cdeeaa4
Steven Than <steventhan11@gmail.com>
2019-03-04 00:20:14 -0500

    made Map component positioned absolute

--- src/components/Explore/Map.js
+++ src/components/Explore/Map.js
@@ -3,6 +3,11 @@ import GoogleMapReact from 'google-map-react';
 
 import mapStyles from './mapStyles.json';
 
+const mapContainerStyle = {
+  height: '100%',
+  width: '100%'
+};
+
 class Map extends Component {
   state = {
     center: {
@@ -13,22 +18,27 @@ class Map extends Component {
   };
 
   handleApiLoaded = (map, maps) => {
-    console.log(map, maps);
     map.setOptions({
-      styles: mapStyles
+      styles: mapStyles,
+      fullscreenControlOptions: {
+        position: maps.ControlPosition.BOTTOM_RIGHT
+      }
     });
   };
 
   render() {
     return (
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: 'AIzaSyD6S0fXIE22l64bV3nhv-EB-7K1gh39nqs' }}
-        defaultCenter={this.state.center}
-        defaultZoom={this.state.zoom}
-        yesIWantToUseGoogleMapApiInternals
-        onGoogleApiLoaded={({ map, maps }) => this.handleApiLoaded(map, maps)}
-      />
+      <div style={mapContainerStyle} className="position-absolute">
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyD6S0fXIE22l64bV3nhv-EB-7K1gh39nqs' }}
+          defaultCenter={this.state.center}
+          defaultZoom={this.state.zoom}
+          yesIWantToUseGoogleMapApiInternals
+          onGoogleApiLoaded={({ map, maps }) => this.handleApiLoaded(map, maps)}
+        />
+      </div>
     );
   }
 }
+
 export default Map;
