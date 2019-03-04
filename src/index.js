8bd6eccc17ed027731ca168c6ca57338e2c99aa4
Steven Than <steventhan11@gmail.com>
2019-03-04 00:30:02 -0500

    added stylings for library

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import { Provider } from 'react-redux';
 import configureStore from './store';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
+import 'react-typist/dist/Typist.css';
+import './index.scss';
 
 ReactDOM.render(
   <Provider store={configureStore()}>
