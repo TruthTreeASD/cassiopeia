054c53165a8f1ddcf1f772dc64fbc946ba333666
Steven Than <steventhan11@gmail.com>
2019-03-04 00:18:24 -0500

    added About component

---
+++ src/components/About/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Container, Row, Col } from 'reactstrap';
+
+class About extends Component {
+  render() {
+    return (
+      <Container fluid>
+        <Row>
+          <Col>About TruthTree</Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default About;
