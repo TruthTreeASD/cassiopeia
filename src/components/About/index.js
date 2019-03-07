727d356e22d3a2caee6fb830825c47c007a85ef9
Steven Than <steventhan11@gmail.com>
2019-03-07 01:41:29 -0500

    removed unused imports in About component

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
-import { Container, Row, Col } from 'reactstrap';
+import { Container, Jumbotron } from 'reactstrap';
+import '../../styles/About.css';
+
+const aboutStyle = {
+  paddingTop: 70
+};
 
 class About extends Component {
   render() {
     return (
-      <Container fluid>
-        <Row>
-          <Col>About TruthTree</Col>
-        </Row>
+      <Container fluid style={aboutStyle} className="about">
+        <Jumbotron className="float-center">
+          <h1 className="display-3">TruthTree</h1>
+          <p>A Public Data Vizipedia/Wiki-Laboratory</p>
+          <hr className="my-2" />
+          <p className="lead">
+            TruthTree seeks to unite all the public data in the world into a
+            data-visualization tool and reference platform that can facilitate
+            data-driven understanding and consensus regarding geography,
+            politics, and culture for geographical entities at all scales.
+          </p>
+          <p className="lead" />
+        </Jumbotron>
       </Container>
     );
   }
