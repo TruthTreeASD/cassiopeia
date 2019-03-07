06d4d1db094a4d4473e6dc238a1e91c5efe78796
Steven Than <steventhan11@gmail.com>
2019-03-07 02:07:21 -0500

    fixed unit tests failure

--- src/components/Header.js
+++ src/components/Header.js
@@ -35,7 +35,7 @@ class Header extends Component {
   render() {
     return (
       <Navbar style={navbarStyle} expand="md" className="fixed-top">
-        <NavbarBrand style={navBrandStyle}>
+        <NavbarBrand className="text-primary" style={navBrandStyle}>
           <Link to="/">
             <img
               src={logo}
@@ -43,8 +43,8 @@ class Header extends Component {
               className="d-inline-block"
               alt="TruthTree logo"
             />
-            &nbsp;TruthTree
           </Link>
+          &nbsp;TruthTree
         </NavbarBrand>
         <NavbarToggler
           onClick={() =>
