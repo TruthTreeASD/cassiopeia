c5ff391d769a19143dd40d076639215da784c922
Jhalaa <jhalaachinoy@gmail.com>
2019-03-07 07:19:47 -0800

    Fixing up attribute selection

--- src/components/DisplayComponent.js
+++ src/components/DisplayComponent.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Grid } from 'react-virtualized';
 import { connect } from 'react-redux';
 import axios from 'axios/index';
 import _ from 'lodash';
@@ -18,26 +17,17 @@ class DisplayComponent extends Component {
     super(props);
     this.state = {
       currentLevel: null,
-      data: [['Name', 'Population']],
+      data: {},
       locationIds: [],
       dropdownOpen: false,
       selectedNormalization: 'No attribute',
       normalizationValues: []
     };
-    this.cellRenderer = this.cellRenderer.bind(this);
     this.getAttributeType = this.getAttributeType.bind(this);
     this.populationRangeCall = this.populationRangeCall.bind(this);
     this.normalizationValuesCall = this.normalizationValuesCall.bind(this);
     this.toggle = this.toggle.bind(this);
     this.setSelectedNormalization = this.setSelectedNormalization.bind(this);
-  }
-
-  cellRenderer({ columnIndex, key, rowIndex, style }) {
-    return (
-      <div key={key} style={style}>
-        {this.state.data[rowIndex][columnIndex]}
-      </div>
-    );
   }
 
   getAttributeType(type) {
@@ -49,7 +39,7 @@ class DisplayComponent extends Component {
 
   componentDidMount() {
     this.setState(prevState => ({
-      data: [['Name', 'Population']]
+      data: {}
     }));
     this.normalizationValuesCall();
     this.populationRangeCall();
@@ -82,7 +72,7 @@ class DisplayComponent extends Component {
   populationRangeCall() {
     let minPopulation = 0;
     let maxPopulation = 0;
-    let data = [['Name', 'Population']];
+    let data = {};
     let locationIds = [];
     let year = this.props.yearSelected ? this.props.yearSelected : 2016;
     // Calculate min and max population
@@ -110,7 +100,7 @@ class DisplayComponent extends Component {
           )
           .then(response => {
             _.map(response.data, obj => {
-              data.push([obj.name, obj.population]);
+              data[obj.id] = { name: obj.name, '1': obj.population };
               locationIds.push(obj.id);
             });
             this.setState({ data: data });
@@ -126,13 +116,8 @@ class DisplayComponent extends Component {
   }
 
   render() {
-    console.log('----------');
-    console.log(this.state.data);
-    console.log('----------');
     let attributes = this.getAttributeType('ids');
     if (attributes.length > 0) {
-      let temp = ['Name', 'Population'];
-      this.state.data[0] = temp.concat(this.getAttributeType('name'));
       axios
         .get(
           '/api/attributes?locationIds=' +
@@ -143,27 +128,11 @@ class DisplayComponent extends Component {
             this.props.year
         )
         .then(response => {
-          //data contains the variables
-          console.log(response.data);
           _.map(response.data, row => {
-            console.log('+++++++++');
-            console.log(
-              this.state.data[this.state.locationIds.indexOf(row.location_id)]
-            );
-            console.log(
-              _.map(row.attributes, elem => {
-                return elem.data;
-              })
-            );
-            console.log('++++++++++');
-            this.state.data[
-              1 + this.state.locationIds.indexOf(row.location_id)
-            ].push(
-              row.attributes[0].data[0].value
-              // _.map(row.attributes, elem => {
-              //     return elem.data[0].value;
-              // })
-            );
+            _.map(row.attributes, elem => {
+              this.state.data[row.location_id][elem.attribute_id] =
+                elem.data[0].value;
+            });
           });
         })
         .catch(error => {
@@ -218,17 +187,16 @@ class DisplayComponent extends Component {
             </tr>
           </thead>
           <tbody>
-            {this.state.data.map((column, index) => {
-              if (index === 0) return;
-              else {
-                return (
-                  <tr key={index}>
-                    {column.map((value, i) => {
-                      return <td key={i}>{value}</td>;
-                    })}
-                  </tr>
-                );
-              }
+            {_.values(this.state.data).map((row, index) => {
+              return (
+                <tr key={index}>
+                  <td>{row['name']}</td>
+                  <td>{row['1']}</td>
+                  {this.props.selectedAttributes.map(column => {
+                    return <td>{row[column[0]]}</td>;
+                  })}
+                </tr>
+              );
             })}
           </tbody>
         </Table>
