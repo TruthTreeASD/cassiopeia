870acb4346f1d35c916bc8cb9f5691e285e22571
Sheela Sathyanarayana <sheelasathyanarayana@gmail.com>
2019-03-08 01:04:10 -0500

    Storing mormalization type in redux

--- src/components/Normalization.js
+++ src/components/Normalization.js
@@ -8,6 +8,8 @@ import {
   DropdownToggle
 } from 'reactstrap';
 import axios from 'axios/index';
+import { connect } from 'react-redux';
+
 import { TRUTHTREE_URI } from '../constants';
 
 class Normalization extends Component {
@@ -15,7 +17,8 @@ class Normalization extends Component {
     super(props);
     this.state = {
       dropdownOpen: false,
-      selectedNormalization: 'Gross',
+      selectedNormalizationDisplayName: 'Gross',
+      selectedNormalizationName: 'GROSS',
       normalizationValues: []
     };
     this.normalizationValuesCall = this.normalizationValuesCall.bind(this);
@@ -33,10 +36,15 @@ class Normalization extends Component {
     }));
   }
 
-  setSelectedNormalization(value) {
-    this.setState(prevState => ({
-      selectedNormalization: value
-    }));
+  setSelectedNormalization(e) {
+    //console.log(e.target.id);
+    //this.setState({selectedNormalizationDisplayName: e.target.value});
+    //this.setState({selectedNormalizationName: e.target.id});
+    this.props.dispatch({
+      type: 'CHANGE_NORMALIZATION',
+      selectedNormalizationName: e.target.id,
+      selectedNormalizationDisplayName: e.target.value
+    });
   }
 
   normalizationValuesCall() {
@@ -52,6 +60,17 @@ class Normalization extends Component {
   }
 
   render() {
+    let values = this.state.normalizationValues.map((value, i) => (
+      <DropdownItem
+        key={i}
+        id={value.name}
+        value={value.displayValue}
+        onClick={this.setSelectedNormalization}
+      >
+        {value.displayValue}
+      </DropdownItem>
+    ));
+
     return (
       <div id="normalisation">
         Normalization attribute:
@@ -61,29 +80,27 @@ class Normalization extends Component {
           toggle={this.toggle}
         >
           <DropdownToggle caret>
-            {this.state.selectedNormalization}
+            {' '}
+            {this.props.selectedNormalizationDisplayName}{' '}
           </DropdownToggle>
-          <DropdownMenu>
-            <DropdownItem
-              onClick={() => this.setSelectedNormalization('Gross')}
-            >
-              Gross
-            </DropdownItem>
-            <DropdownItem
-              onClick={() => this.setSelectedNormalization('Per Capita')}
-            >
-              Per Capita
-            </DropdownItem>
-            <DropdownItem
-              onClick={() => this.setSelectedNormalization('Per Revenue')}
-            >
-              Per Revenue
-            </DropdownItem>
-          </DropdownMenu>
+          <DropdownMenu>{values}</DropdownMenu>
         </Dropdown>
       </div>
     );
   }
 }
+const mapStateToProps = state => {
+  return {
+    selectedNormalizationName:
+      state.NormalizationReducer.selectedNormalizationName,
+    selectedNormalizationDisplayName:
+      state.NormalizationReducer.selectedNormalizationDisplayName
+  };
+};
 
-export default Normalization;
+const mapDispatchToProps = dispatch => ({ dispatch });
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Normalization);
