6c80c2165f48ce61cbec1565b03f9d5f3adcdc2b
Steven Than <steventhan11@gmail.com>
2019-03-10 01:07:59 -0500

    added loading state to LocationSearchBox reducer

--- src/reducers/LocationSearchBoxReducer.js
+++ src/reducers/LocationSearchBoxReducer.js
@@ -1,10 +1,22 @@
-export default (state = { value: '' }, action) => {
+const defaultState = {
+  value: '',
+  loading: true
+};
+
+export default (state = defaultState, action) => {
   switch (action.type) {
     case 'SEARCHBOX_UPDATE_VALUE':
       return {
         ...state,
         value: action.payload
       };
+
+    case 'SEARCHBOX_FINISH_LOADING':
+      return {
+        ...state,
+        loading: false
+      };
+
     default:
       return state;
   }
