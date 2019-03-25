4f6eca4f0dc10ebbb2324bdb4e943293e7b1823c
Sheela Sathyanarayana <sheelasathyanarayana@gmail.com>
2019-03-25 15:00:13 -0400

    react-select

--- src/components/Advance/SimilarPlacesSearch.js
+++ src/components/Advance/SimilarPlacesSearch.js
@@ -11,105 +11,145 @@ import {
   CardHeader,
   Button
 } from 'reactstrap';
+import Select from 'react-select';
+
+const options = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' }
+];
+
+let allYears = [];
+for (let i = 2016; i > 1966; i--) {
+  allYears.push({
+    value: i,
+    label: i
+  });
+}
+
+let numberOfLocationsList = [
+  { value: '1', label: '1' },
+  { value: '2', label: '2' },
+  { value: '3', label: '3' },
+  { value: '4', label: '4' },
+  { value: '5', label: '5' },
+  { value: '6', label: '6' },
+  { value: '7', label: '7' },
+  { value: '8', label: '8' },
+  { value: '9', label: '9' },
+  { value: '10', label: '1' }
+];
+
+const normalizationList = [
+  { value: 'Gross', label: 'Gross' },
+  { value: 'ByPopulation', label: 'By Population' },
+  { value: 'ByRevenue', label: 'By Revenue' }
+];
 
 class SimilarPlacesSearch extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedAttributes: null,
+      numberOfLocations: { value: '1', label: '1' },
+      normalization: { value: 'Gross', label: 'Gross' },
+      yearSelectedMin: { value: '2016', label: '2016' },
+      yearSelectedMax: { value: '2016', label: '2016' }
+    };
+    this.handleChangeAttribute = this.handleChangeAttribute.bind(this);
+    this.handleChangeNumber = this.handleChangeNumber.bind(this);
+    this.handleChangeYearMin = this.handleChangeYearMin.bind(this);
+    this.handleChangeYearMax = this.handleChangeYearMax.bind(this);
+  }
+
+  handleChangeAttribute = selectedAttributes => {
+    this.setState({ selectedAttributes });
+  };
+
+  handleChangeNumber = numberOfLocations => {
+    this.setState({ numberOfLocations });
+  };
+
+  handleChangeNormalization = normalization => {
+    this.setState({ normalization });
+  };
+
+  handleChangeYearMin = yearSelectedMin => {
+    this.setState({ yearSelectedMin });
+  };
+  handleChangeYearMax = yearSelectedMax => {
+    this.setState({ yearSelectedMax });
+  };
+
   render() {
     return (
       <Card>
         <CardHeader>Search for locations with similar trend</CardHeader>
-
         <CardBody>
           <Form>
             <FormGroup row>
-              <Label for="locationOfInterest" xs="2">
-                Location of Interest:
-              </Label>
-              <Col xs="auto">
+              <Label for="locationOfInterest">Location of Interest:</Label>
+              <Col>
                 <Input
                   id="locationOfInterest"
                   placeholder="Search for location"
-                  bsSize="sm"
                 />
               </Col>
             </FormGroup>
+
             <FormGroup row>
-              <Label for="exampleSelect" xs="2">
+              <Label for="selectedAttribues">Selected attributes :</Label>
+              <Col>
+                <Select
+                  value={this.state.selectedOption}
+                  onChange={this.handleChangeAttribute}
+                  options={options}
+                  isMulti="true"
+                />
+              </Col>
+            </FormGroup>
+
+            <FormGroup row>
+              <Label for="numberOfLocations">
+                {' '}
                 Number of similar locations:
               </Label>
-              <Col xs="auto">
-                <Input
-                  type="select"
-                  name="select"
-                  id="exampleSelect"
-                  bsSize="sm"
-                >
-                  <option>1</option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>4</option>
-                </Input>
-              </Col>
-            </FormGroup>
-            <FormGroup row>
-              <Label for="exampleSelect" xs="2">
-                Normalization :
-              </Label>
-              <Col xs="auto">
-                <Input
-                  type="select"
-                  name="select"
-                  id="exampleSelect"
-                  bsSize="sm"
-                >
-                  <option>By Population</option>
-                  <option>By Revenue</option>
-                  <option>Gross</option>
-                </Input>
-              </Col>
-            </FormGroup>
-            <FormGroup row>
-              <Label for="exampleSelect" xs="2">
-                Year Range :
-              </Label>
-              <Col xs="auto">
-                <Input
-                  type="select"
-                  name="select"
-                  id="exampleSelect"
-                  bsSize="sm"
-                >
-                  <option>2016</option>
-                  <option>2015</option>
-                  <option>2014</option>
-                </Input>
-              </Col>
-              <Col xs="auto"> - </Col>
-              <Col xs="auto">
-                <Input
-                  type="select"
-                  name="select"
-                  id="exampleSelect"
-                  bsSize="sm"
-                >
-                  <option>2016</option>
-                  <option>2015</option>
-                  <option>2014</option>
-                </Input>
-              </Col>
-            </FormGroup>
-            <FormGroup row>
-              <Label for="exampleSelect" xs="2">
-                Selected attributes :
-              </Label>
-              <Col xs="auto">
-                <Input
-                  type="textarea"
-                  name="select"
-                  id="exampleSelect"
-                  bsSize="sm"
+              <Col xs="1">
+                <Select
+                  value={this.state.numberOfLocations}
+                  onChange={this.handleChangeNumber}
+                  options={numberOfLocationsList}
                 />
               </Col>
+
+              <Label for="normalizationType"> Normalization :</Label>
+              <Col xs="2">
+                <Select
+                  value={this.state.normalization}
+                  onChange={this.handleChangeNormalization}
+                  options={normalizationList}
+                />
+              </Col>
+
+              <Label for="year">Year:</Label>
+              <Col xs="1">
+                {' '}
+                <Select
+                  value={this.state.yearSelectedMin}
+                  onChange={this.handleChangeYearMin}
+                  options={allYears}
+                />{' '}
+              </Col>
+              <Col xs="auto"> - </Col>
+              <Col xs="1">
+                <Select
+                  value={this.state.yearSelectedMax}
+                  onChange={this.handleChangeYearMax}
+                  options={allYears}
+                />{' '}
+              </Col>
             </FormGroup>
+
             <Button>Submit</Button>
           </Form>
         </CardBody>
