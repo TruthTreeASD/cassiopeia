285b080467cfa5202fe7d7f9d72c48e19ed983dd
Steven Than <steventhan11@gmail.com>
2019-04-06 23:47:51 -0400

    added SideMenu component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import Home from './components/Home';
 import Header from './components/Header';
 import About from './components/About';
 import Explore from './components/Explore';
 import Stories from './components/Stories';
 import Admin from './components/Admin';
+import SideMenu from './components/SideMenu';
 
 import './App.css';
 import './styles/Tab.css';
@@ -16,6 +18,7 @@ class App extends Component {
       <Router>
         <div className="d-flex flex-column">
           <Header />
+          <SideMenu />
           <Switch>
             <Route exact path="/" component={Explore} />
             <Route exact path="/stories" component={Stories} />
