9de8d096b9c84de249f478b0a231e7fac504f3e1
Neha <shukla.ne@husky.neu.edu>
2019-04-08 22:46:50 -0400

    changed server url

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,4 @@
-// const TRUTHTREE_URI = 'https://truthtree.herokuapp.com';
-const TRUTHTREE_URI = 'http://localhost:8080';
+const TRUTHTREE_URI = 'https://truthtree.herokuapp.com';
 module.exports = {
   TRUTHTREE_URI
 };
